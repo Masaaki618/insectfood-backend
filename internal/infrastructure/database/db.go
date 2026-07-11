18aa13cc7e47a795a85494d1341a32a38eb6248a
Masaaki618 <masaaki_618@icloud.com>
2026-07-11 22:21:03 +0900

    更新: DB接続にDB_TLS環境変数でTLS設定を切り替える機能を追加

--- internal/infrastructure/database/db.go
+++ internal/infrastructure/database/db.go
@@ -10,12 +10,17 @@ import (
 
 // NewDB は環境変数からDSNを組み立ててGORMのDB接続を生成する
 func NewDB() (*gorm.DB, error) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=True&loc=Asia%%2FTokyo",
+	tls := os.Getenv("DB_TLS")
+	if tls == "" {
+		tls = "false"
+	}
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=True&loc=Asia%%2FTokyo&tls=%s",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_NAME"),
+		tls,
 	)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
